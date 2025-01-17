ab68d437f3b9e724fb64bb8924db6156d945addc
lvingonzales <lvingonzales15@gmail.com>
2025-01-17 12:24:52 -0500

    Added csv to the assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -30,7 +30,7 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|csv)$/i,
         type: "asset/resource",
       },
     ],
