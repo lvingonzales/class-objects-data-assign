74094f6d8a3924a6110da43a05963228593334cc
lvingonzales <lvingonzales15@gmail.com>
2025-01-17 11:56:31 -0500

    Finished the charts, data and descriptions

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import drawHowChart from "./chart_how";
 import drawWhereChart from "./chart_where";
 import "./style.css";
 
 const whereChart = document.querySelector(".where > .chart");
-const howChart = document.querySelector(".how");
+const howChart = document.querySelector(".how > .chart");
 
 drawWhereChart().then(svgNode => {
     whereChart.append(svgNode);
 });
+
+drawHowChart().then(svgNode => {
+    howChart.append(svgNode);
+})
