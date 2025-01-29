987e555fc4c8053d75973f52c4f285316a72db00
lvingonzales <lvingonzales15@gmail.com>
2025-01-29 13:37:51 -0500

    added scaling animations to the visualizations

--- src/chart_histo.js
+++
@@ -1,8 +0,0 @@
-import * as d3 from "d3";
-
-export default async function drawHistogram () {
-
-    const data = await data.csv("../A1_histo_dataset.csv");
-
-    return svg.node();
-}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import drawBarChart from "./chart_bar";
 import drawHowChart from "./chart_how";
 import drawWhereChart from "./chart_where";
 import "./style.css";
 
 const whereChart = document.querySelector(".where > .chart");
 const howChart = document.querySelector(".how > .chart");
+const barChart = document.querySelector(".bar > .chart");
 
 drawWhereChart().then(svgNode => {
     whereChart.append(svgNode);
